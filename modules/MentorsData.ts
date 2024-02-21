ca6be6fbe560063232db036b0f67a59f25ff7434
Lasya <44485522+lasyaneti@users.noreply.github.com>
2024-02-21 17:36:46 -0600

    Update MentorsData.ts

--- modules/MentorsData.ts
+++ modules/MentorsData.ts
@@ -19,178 +19,180 @@ export const mentors = [
     },
     {
         id: 4,
+        name: "Dev Bharel",
+        image: "mentors/mentor-images/dev.jpg",
+        desc: "A serial hackathoner, Dev has been working in crypto since 2013 and worked across multiple ecosystems. His passion is game design."
+    },
+    {
+        id: 5,
         name: "Gino P. Corrales",
         image: "mentors/mentor-images/gino.jpg",
         desc: "I am so proud to be in many of the past Hackillinois as a participant and mentor. I studied System Engineering back at home in Peru, later I went to ISU for Computer Science. Currently I am doing a master’s in management at the University of Illinois UIUC. I interned at State Farm as a mobile developer and later as a business/technical team lead.  I embarked on my journey within Caterpillar since 2013. I started as a System Analyst working with ERP systems, eventually moving to the Cybersecurity division.  Currently, I lead the Identity Access Management support team. I take pride in leading efforts to respond to customer issues, requests, strengthen backend processes and services."
     },
     {
-        id: 5,
+        id: 6,
         name: "Josh Carrington",
         image: "mentors/mentor-images/josh.jpg",
         desc: "I am an IT professional with nearly a decade of experience. I’m currently working as a system admin supporting Cat Digital’s eCommerce application. I’m experienced with Linux, AWS, Docker, and Kubernetes. Reach out on Discord! I look forward to seeing the projects you all develop!"
     },
     {
-        id: 6,
+        id: 7,
         name: "Saba Imran",
         image: "mentors/mentor-images/saba.jpg",
         desc: "After working at Microsoft and Two Sigma for a couple of years, I moved onto the world of startups. I'm currently the Founder/CTO of Khoj, a company backed by YCombinator. We make it easier for people to connect their personal notes and docs with AI."
     },
     {
-        id: 7,
+        id: 8,
         name: "Jeff Bezanson",
         image: "mentors/mentor-images/jeff.jpg",
         desc: "Jeff is a co-creator of the Julia programming language and co-founder of JuliaHub, Inc. Generally interested in systems, runtimes, compilers, technical/scientific computing, and all things lisp-family!"
     },
     {
-        id: 8,
+        id: 9,
         name: "Susan Hinrichs",
         image: "mentors/mentor-images/susan.jpg",
         desc: "Susan specializes in networking, security, and system development. She is currently Chief Scientist at Aviatrix driving efforts with their Distributed Cloud Firewall. Susan is also a member of the Apache Software Foundation and a committer on the Traffic Server project. She did her undergraduate studies at UIUC and earned a PhD in CS from CMU. Susan taught computer and network security at UIUC for a few years while working on various startup and freelancing projects."
     },
     {
-        id: 9,
+        id: 10,
         name: "Advait Maybhate",
         image: "mentors/mentor-images/advait.jpg",
         desc: "Advait Maybhate is a Software Engineer at Warp. He's a Computer Science graduate from the University of Waterloo. In the past, he's completed internships at Ramp, Google, Riot Games and Octant Bio. His interests include developer tools, bioinformatics and EdTech. He's also an avid hackathoner where he's been a hacker, organizer, mentor, sponsor and judge!"
     },
     {
-        id: 10,
+        id: 11,
         name: "Pengcheng Xu",
         image: "mentors/mentor-images/pengcheng.jpg",
         desc: "I am Pengcheng Xu. I am a master's student in ECE at UIUC. ex Amazon SDE intern. open-source contributer to OpenMLDB.  ex Deep Learning Software engineer Intern at Intel.  I have research experience in artificial intelligence for bioinformatics and molecular optimization."
     },
     {
-        id: 11,
+        id: 12,
         name: "Jason R Coombs",
         image: "mentors/mentor-images/jason.jpg",
         desc: "Jason is a Software Engineering Manager at Google, recently working on the Next Generation of source control at Google based on martinvonz/jj. Jason is very active in the Python open source community and a maintainer or direct contributor to CPython, CherryPy, setuptools, and others."
     },
     {
-        id: 12,
+        id: 13,
         name: "Anthony Avina",
         image: "mentors/mentor-images/anthony.jpg",
         desc: "UIUC CS Alumnus from over a decade ago. I'm a Senior Software Engineer at an InsurTech startup, Reserv. I mainly work on full stack features on Ruby on Rails and Python + Django applications. I'm currently living in Chicago with my partner and our mixed-breed dog, Cora. Mentoring is one of my favorite things to do so I can't wait to participate with you all!"
     },
     {
-        id: 13,
-        name: "Saransh Bhardwaj",
-        image: "mentors/mentor-images/saransh.jpg",
-        desc: "As a Staff Engineer, I specialize in developing large-scale applications across various platforms, including APIs, single-page apps, and Android applications. My expertise also extends to integrating and interacting with Large Language Models, enabling advanced language processing capabilities within various projects. Feel free to ask me anything related to best practices, bootstrapping applications, scaling or any other general tech questions."
-    },
-    {
         id: 14,
-        name: "Matthew Weston",
-        image: "mentors/mentor-images/matthew.jpg",
-        desc: "Hello. I'm a PhD student studying machine learning. If your project has an ML component, I'd be especially interested in lending a hand."
-    },
-    {
-        id: 15,
         name: "David An",
         image: "mentors/mentor-images/david.jpg",
         desc: "Hi! I am a recent alum of Illinois and have been traveling around to various hackathons being a mentor and judge. My current work involves developing scalable and secure systems. In my free time, I enjoy biking and running! Feel free to ask me about anything related to full stack or system design!"
     },
     {
+        id: 15,
+        name: "Saransh Bhardwaj",
+        image: "mentors/mentor-images/saransh.jpg",
+        desc: "As a Staff Engineer, I specialize in developing large-scale applications across various platforms, including APIs, single-page apps, and Android applications. My expertise also extends to integrating and interacting with Large Language Models, enabling advanced language processing capabilities within various projects. Feel free to ask me anything related to best practices, bootstrapping applications, scaling or any other general tech questions."
+    },
+    {
         id: 16,
+        name: "Matthew Weston",
+        image: "mentors/mentor-images/matthew.jpg",
+        desc: "Hello. I'm a PhD student studying machine learning. If your project has an ML component, I'd be especially interested in lending a hand."
+    },
+    
+    {
+        id: 17,
         name: "Toyosi Bamidele",
         image: "mentors/mentor-images/toyosi.jpg",
         desc: "Toyosi Bamidele is an accomplished Data Scientist at Visa Consulting and Analytics, known for enhancing analytical capabilities and decision-making processes. Previously at Truist Financial, Toyosi's work centered on big data and modeling. Holding a Master's in Data Science from Texas Tech University, and a background in Petroleum and Natural Gas Engineering from Pennsylvania State University, Toyosi is distinguished by a unique blend of technical knowledge and strategic business insights."
     },
     {
-        id: 17,
+        id: 18,
         name: "Aritra Ghosh",
         image: "mentors/mentor-images/aritra.jpg",
         desc: "Aritra is a product manager at Microsoft. He has experience working at both startups and large companies. In his free time, he likes tinkering with software and playing squash."
     },
     {
-        id: 18,
+        id: 19,
         name: "Minu Mathew",
         image: "mentors/mentor-images/minu.jpg",
         desc: "Hi! I'm a Research Software Engineer at the National Center for Supercomputing Applications(NCSA) at UIUC. I have a Masters in Computer Science and a Masters in Wireless Communication and I try to utilize the knowledge gained across multiple verticals to deliver better solutions and software. My primary interests are in the field of ML/AI with a focus in NLP. At NCSA, I work on multiple projects ranging from implementing LLM agents for RNA Sequencing tasks to development of a cloud native data management framework. Outside of work, I like badminton, swimming, traveling, painting and attending musical concerts."
     },
     {
-        id: 19,
+        id: 20,
         name: "Krishna Chaitanya Rao Kathala",
         image: "mentors/mentor-images/krishna.jpg",
         desc: "Krishna Chaitanya Rao Kathala is a seasoned Data Science/Analytics & AI expert and a PhD Candidate at the University of Massachusetts Amherst. With over 6+ years of expertise, he specializes in Data Analytics, Machine Learning, and Artificial Intelligence. Co-authored Machine Learning and Data Science books, Krishna holds 5+ international patents and has published 8+ research papers. An active reviewer and contributor to critical fields like AI, Data Science, Analytics and NLP, he has judged 10+ national and international events. A sought-after speaker, Krishna is committed to innovation and leadership in the technology domain."
     },
     {
-        id: 20,
+        id: 21,
         name: "Anusha Ghosh",
         image: "mentors/mentor-images/anusha.jpg",
         desc: "grad student @ UIUC. working at the intersection of AI and cybersecurity. let's build something fun together :D"
     },
     {
-        id: 21,
+        id: 22,
         name: "T. Andrew Manning",
         image: "mentors/mentor-images/manning.jpg",
         desc: "Andrew Manning is a senior research scientist at the National Center for Supercomputing Applications (NCSA) at the University of Illinois. His physics background informs his role as a research software engineer in the design and construction of cyberinfrastructure and applications for a variety of research projects and collaborations."
     },
     {
-        id: 22,
+        id: 23,
         name: "Bill Siever",
         image: "mentors/mentor-images/bill.jpg",
         desc: "Bill has experience in C.S. (B.S. and M.S.) and Comp. Eng (PhD).  He teaches courses in computer engineering/embedded, IoT, and introductory C.S..  Consulting work includes developing Bluetooth devices and firmware for medical devices."
     },
     {
-        id: 23,
+        id: 24,
         name: "Ana Elissa Cabrera",
         image: "mentors/mentor-images/ana.jpg",
         desc: "Ana Elissa is a Master's in Computer Science student at Illinois. She previously studied political science and linguistics at UIUC and transitioned into CS after graduation. Her specialty is programming languages and software engineering. A.E. is motivated to offer guidance to students in non-CS majors interested in pursuing computer science in graduate school! Her first experience in HackIllinois was as a non-STEM undergraduate student in 2020 and she knows first-hand how daunting your first Hackathon can be in a new field that may be outside of your major or comfort zone. A.E. is here to help answer your technical and post-college questions!"
     },
     {
-        id: 24,
+        id: 25,
         name: "Prahathess",
         image: "mentors/mentor-images/prahathess.jpg",
         desc: "I am a cybersecurity leader with 10 years of industry experience specializing in Application and Infrastructure security"
     },
     {
-        id: 25,
+        id: 26,
         name: "Brett Koonce",
         image: "mentors/mentor-images/brett.jpg",
         desc: "Brett Koonce grew up on a farm in the midwest where he learned to code at an early age. He attended USC film school and later the University of Missouri for a master's degree in computer science. He is passionate about neural networks, writing a book on the subject in 2021. He is the CTO/co-founder of hallway (joinhallway.com), which enables creators to connect with their audiences at scale!"
     },
     {
-        id: 26,
+        id: 27,
         name: "Tania Chakraborty",
         image: "mentors/mentor-images/tania.jpg",
         desc: "I'm a grad student at Purdue and my research area is NLP."
     },
     {
-        id: 27,
+        id: 28,
         name: "Herat Joshi",
         image: "mentors/mentor-images/herat.jpg",
         desc: "Herat Joshi, a seasoned Healthcare Technology Manager and Data Intelligence Architect, excels in integrating AI into healthcare, driving technological advancements and innovation in project management and data analysis."
     },
     {
-        id: 28,
+        id: 29,
         name: "Candice Hou",
         image: "mentors/mentor-images/candice.jpg",
         desc: "Hi, I'm Candice Hou. I'm currently a Product Manager at TikTok. I used to work at Google as a Solution Engineer. I'm happy to talk about product design, cloud solution architecting, full-stack development."
     },
     {
-        id: 29,
+        id: 30,
         name: "Rob Miroballi",
         image: "mentors/mentor-images/rob.jpg",
         desc: "Principal Architect, Solutions Developer, working on applications used in enterprise settings. Specialties: Architecture Development, Enterprise Development, Analysis and Design, Distributed Systems, SOA, Integration, Custom Production Support Tools."
     },
     {
-        id: 30,
+        id: 31,
         name: "Muhammad Hassan",
         image: "mentors/mentor-images/muhammad.jpg",
         desc: "My research area is the identification and mitigation of security and privacy issues in smart technologies and applications, such as web, IoT, and smartphone platforms. I work on the following domains: digital health solutions, financial scam and cybercrime prevention in smartphone and web applications, and phishing and user-tracking detection in online social media."
     },
     {
-        id: 31,
+        id: 32,
         name: "André Bauer",
         image: "mentors/mentor-images/andre.jpg",
         desc: "Dr. André Bauer did his Ph.D. at the University of Würzburg from 2016 to 2020, achieving Summa Cum Laude honors. His doctoral thesis received both national and international recognition, earning prestigious awards. In January 2021, Dr. Bauer took on a postdoctoral role at the University of Würzburg, followed by a position at the University of Chicago in November 2022. He is a passionate advocate for science communication and an attending speaker at events like the South Side Science Festival. In July 2021, Dr. Bauer founded the SPEC RG Predictive Data Analytics Group. His research spans Performance Engineering and Data Science, focusing on unlocking the potential of data science in scientific computing."
     },
-    {
-        id: 32,
-        name: "Dev Bharel",
-        image: "mentors/mentor-images/dev.jpg",
-        desc: "A serial hackathoner, Dev has been working in crypto since 2013 and worked across multiple ecosystems. His passion is game design."
-    },
+    
     {
         id: 33,
         name: "Kavita Cardozo",
